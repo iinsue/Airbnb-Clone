154f77169bd6c4fe611f0f9df2fcf26b05a4b917
iinsue <hinsu93@naver.com>
2020-07-08 15:45:35 +0900

    #3.3 Finishing User Model

--- users/models.py
+++ users/models.py
@@ -18,8 +18,27 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other"),
     )
 
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
     avatar = models.ImageField(null=True, blank=True)  # null 허용
     gender = models.CharField(
         choices=GENDER_CHOICES, max_length=10, null=True, blank=True
     )  # 최대글자수 제한
     bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+    )
+
+    superhost = models.BooleanField(default=False)
